a240251868b0f1f233f76e57d1052fe7a488fda7
ChoChihTun <33831669+ChoChihTun@users.noreply.github.com>
2018-05-18 21:23:17 +0800

    Add adding todo feature

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
-// Check off specific Todos by clicking
-$("li").click(function() {
+// Checks off specific todo by clicking
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
-// Click on X to delete Todo
-$("span").click(function(event) {
+// Clicks on X to delete todo
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove(); //Parent element
     });
     event.stopPropagation();
+});
+
+// Adds new todo to the list
+$("input[type='text'").keypress(function(event) {
+    if (event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+        $(this).val("");
+    }
 });
